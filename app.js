d72672950590f76538a8045fe357fb84b98857bf
Daniel W <whiteside.danielj@gmail.com>
2022-07-18 16:12:46 -0500

    style homework button and add contact info

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const mathWeek1Button = document.querySelector('#math-week-1-button');
 
 mathWeek1Button.addEventListener('click', () =>{
-   alert('Homework: Complete Official SAT Practice Test #1, both math sections (calc and non calc). \n \nDue date: Send your completed answer choices for both sections to Daniel via email before end of day on Sunday July 24th. Answer document must be a photo/scan of your paper answer key or screenshot from your Khan score report. Please complete Section 3 (Non Calculator) and Section 4 (Calculator) before Sunday July 24th.');
+   alert('Homework: Complete Official SAT Practice Test #1, both math sections (calc and non calc). \n \nDue date: Send your completed answer choices for both sections to Daniel via email before end of day on Sunday July 24th. Answer document must be a photo/scan of your paper answer key or screenshot from your Khan score report. Please complete Section 3 (Non Calculator) and Section 4 (Calculator) before Sunday July 24th. \n \nYou can access SAT Official Practice test via khanacademy.org or CollegeBoard official site. You can also just google "SAT Practice Test #1"');
 });
 
